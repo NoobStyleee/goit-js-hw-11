e3c62fb2ae94fa3ee5599f0b538b9bdf73bc5ba4
Rödelbronn Markisen - M. Rödelbronn <Michael Rdelbronn@RB-Markisen.local>
2026-03-17 16:39:45 +0100

    finished hw

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '55024510-6eda0fb880012efb9d7a730f7';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  });
+
+  return response.data;
+}
